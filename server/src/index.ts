23171ef3c8b0768a21ebb4093f1e4b3bb56df263
Sathyam Sahu <sathyamrock55@gmail.com>
2024-11-12 15:49:57 +0530

    main server almost done

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { userRouter } from "./router/user";
+import { zapRouter } from "./router/zap";
+import cors from "cors";
+import { triggerRouter } from "./router/trigger";
+import { actionRouter } from "./router/action";
+
+const app = express();
+const port = 4000;
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/v1/user", userRouter);
+
+app.use("/api/v1/zap", zapRouter);
+
+app.use("/api/v1/trigger", triggerRouter);
+
+app.use("/api/v1/action", actionRouter);
+
+app.listen(port, () => {
+    console.log("Main server running on port", port); 
+});
